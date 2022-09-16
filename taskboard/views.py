9b983393046eb645c4252b95c13e27eb5b18dc90
Fredi Wilkens <frediwilkens@gmail.com>
2022-09-16 15:01:52 -0300

    feat: Create models, populate DB and create API

--- taskboard/views.py
+++ taskboard/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Board, Task
+from .serializer import BoardSerializer, TaskSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+
+class BoardViewSet(viewsets.ModelViewSet):
+    queryset = Board.objects.all()
+    serializer_class = BoardSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
